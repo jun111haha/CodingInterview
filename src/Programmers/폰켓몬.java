6277474f3cdf67990ac31ba159cb069013eba008
kj <jun111haha@gmail.com>
2022-08-01 00:14:59 +0900

    07.31

---
+++ src/Programmers/폰켓몬.java
@@ -0,0 +1,25 @@
+package Programmers;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class 폰켓몬 {
+    public static void main(String[] args) {
+        int [] nums = {3,1,2,3};
+        System.out.println(solution(nums));
+
+    }
+
+    // 중복을 제거한 셋의 크기가 max보다 크면 max를, 작으면 numsSet의 size를 리턴
+    public static int solution(int[] nums) {
+        int answer = 0;
+        int max  = nums.length / 2;
+
+        HashSet<Integer> hashSet = new HashSet<>();
+        for(int i =0; i<nums.length; i++){
+            hashSet.add(nums[i]);
+        }
+
+       return hashSet.size() > max ? max : hashSet.size();
+    }
+}
