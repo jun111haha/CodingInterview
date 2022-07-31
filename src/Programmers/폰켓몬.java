f2116f133008a0865f49d4840c516ca7efb8225b
kj <jun111haha@gmail.com>
2022-08-01 00:27:52 +0900

    07.31

--- src/Programmers/폰켓몬.java
+++ src/Programmers/폰켓몬.java
@@ -10,7 +10,7 @@ public class 폰켓몬 {
 
     }
 
-    // 중복을 제거한 셋의 크기가 max보다 크면 max를 리턴 작으면 hashSet size를 리턴f
+    // 중복을 제거한 셋의 크기가 max보다 크면 max를 리턴 작으면 hashSet size를 리턴
     public static int solution(int[] nums) {
         int max  = nums.length / 2;
 
