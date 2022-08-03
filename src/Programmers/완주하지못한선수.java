a4e46be6ef2e0dae5dc309df143b00bd583b1279
kj <jun111haha@gmail.com>
2022-08-04 00:25:35 +0900

    8.04

--- src/Programmers/완주하지못한선수.java
+++ src/Programmers/완주하지못한선수.java
@@ -1,7 +1,6 @@
 package Programmers;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class 완주하지못한선수 {
     public static void main(String[] args) {
@@ -11,6 +10,12 @@ public class 완주하지못한선수 {
         System.out.println(solution(participant, completion));
 
     }
+
+    /*
+    * 참가자 해시맵에 getOrDefault 메서드 활용 값을 넣어준다.
+    * 완주자는 해시맵에서 get 해 값을 -1 해준다.
+    * 0 값이 아닌값만 answer 에 넣어주고 return (완주하지 못한 사람)
+    * */
     public static String solution(String[] participant, String[] completion) {
         String answer = "";
 
