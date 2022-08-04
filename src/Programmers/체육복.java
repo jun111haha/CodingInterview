1f5671f887794b0be16b446a1a2ab5482a77fd61
kj <jun111haha@gmail.com>
2022-08-05 00:22:40 +0900

    8.05

--- src/Programmers/체육복.java
+++ src/Programmers/체육복.java
@@ -25,15 +25,15 @@ public class 체육복 {
         for(int i = 0; i< people.length; i++){
             //잃어버리기만 한사람
             if(people[i] == -1){
-                //i-1 >= 0 index 0번째가 아닌 오류 방지 여분이 남는사람
+                //i-1 >= 0 0번째 X index 오류 방지 여분이 남는사람
                 if(i-1 >= 0 && people[i-1] == 1){
-                    people[i]++;
-                    people[i-1]--;
-                //i+1 < people.length 0번째 index 오류 방지 여분이 남는사람
+                    people[i]++;   //받은사람
+                    people[i-1]--; //빌려주기
+                //i+1 < people.length 0번째 O index 오류 방지 여분이 남는사람
                 }else if(i+1 < people.length && people[i+1] == 1){
-                    people[i]++;
-                    people[i+1]--;
-                //찾지 못했다면 체육복을 못입은사람.
+                    people[i]++;   //받은사람
+                    people[i+1]--; //빌려주기
+                //찾지 못했다면 체육복을 못입은사람 이니까 -- 처리
                 }else{
                     answer--;
                 }
