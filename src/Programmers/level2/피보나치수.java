63c1689c3b5164cbba87c30d5d5440506b4090b9
kj <jun111haha@gmail.com>
2022-09-21 00:04:38 +0900

    9.20

---
+++ src/Programmers/level2/피보나치수.java
@@ -0,0 +1,26 @@
+package Programmers.level2;
+
+public class 피보나치수 {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(solution(n));
+    }
+
+    public static int solution(int n) {
+        int[] answer = new int[n + 1];
+        for(int i =0; i<answer.length; i++){
+            if(i == 0){
+                answer[i] = 0;
+            }else if(i == 1){
+                answer[i] = 1;
+            }else{
+                int sum = answer[i - 2] + answer[i - 1];
+                answer[i] = sum % 1234567;
+            }
+
+        }
+        return answer[n];
+    }
+}
+
+
