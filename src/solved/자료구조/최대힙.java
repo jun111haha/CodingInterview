56656d7b2aabe7f079808a0cd1e0e8058e2bfe76
kj <jun111haha@gmail.com>
2022-09-30 00:12:52 +0900

    9.30

---
+++ src/solved/자료구조/최대힙.java
@@ -0,0 +1,28 @@
+package solved.자료구조;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class 최대힙 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int test_case = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i =0; i<test_case; i++){
+            int n = Integer.parseInt(br.readLine());
+            if(n == 0){
+                if(!priorityQueue.isEmpty()){
+                    System.out.println(priorityQueue.poll());
+                }else{
+                    System.out.println(0);
+                }
+                continue;
+            }
+            priorityQueue.add(n);
+        }
+    }
+}
