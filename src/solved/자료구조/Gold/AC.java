2118b8b013c2c17d5c62862e0b062dcf17b942f4
kj <jun111haha@gmail.com>
2022-10-06 00:03:30 +0900

    10.05

--- src/solved/자료구조/Gold/AC.java
+++ src/solved/자료구조/Gold/AC.java
@@ -1,83 +1,82 @@
 package solved.자료구조.Gold;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.io.*;
+import java.util.*;
 
 public class AC {
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    static ArrayDeque<Integer> deque;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
 
         int testCase = Integer.parseInt(br.readLine());
         for(int i =0; i<testCase; i++){
-            Deque<Integer> deque = new LinkedList<>(); // 배열을 담을 큐
-            boolean direction = true; // 순방향 || 역방향
-            boolean error = false;
+            String p = br.readLine();
+            int number = Integer.parseInt(br.readLine());
 
-            String p = br.readLine(); // 커맨드
-            int n = Integer.parseInt(br.readLine()); // 배열 원소 수
-            String temp = br.readLine(); // 배열 원소
-            temp = temp.substring(1, temp.length() - 1);
-            String [] arr = temp.split(",");
+            StringTokenizer st = new StringTokenizer(br.readLine(),"[],");
+            deque = new ArrayDeque<>();
 
-            if (temp != "") {
-                for (int j = 0; j < arr.length; j++) { // 배열 큐에 담기
-                    deque.add(Integer.parseInt(arr[j]));
-                }
+            for(int j=0; j<number; j++) {
+                deque.add(Integer.parseInt(st.nextToken()));
             }
 
-            for (int j = 0; j < p.length(); j++) {
-                switch (p.charAt(j)) {
-                    case 'R':
-                        direction = !direction;
-                        break;
+            AC(p);
+        }
 
-                    case 'D':
-                        if (!deque.isEmpty()) {
-                            if (direction) { // 순방향
-                                deque.pollFirst();
-                            } else { // 역방향
-                                deque.pollLast();
-                            }
-                        } else {
-                            error = true;
-                        }
-                        break;
-                    default:
-                        break;
-                }
+        System.out.println(sb);
+    } // End of main
 
-                // 비어있는 덱 원소 삭제 시도시
-                if (error)
-                    break;
+    static void AC(String p) {
+        boolean forward_direction = true;
+
+        for(char function : p.toCharArray()) {
+
+            if(function == 'R') {
+                forward_direction = !forward_direction;
+                continue;
             }
 
-            if (error) { // 비어있는 덱 원소 삭제 시도시
-                sb.append("error").append("\n");
+            // 정방향일 때
+            if( forward_direction ) {
+
+                // 덱이 비었으면,
+                if(deque.pollFirst() == null) {
+                    sb.append("error\n");
+                    return;
+                }
+            // 역방향 일때 forward_direction = true
             } else {
-                sb.append("[");
-                if (direction) {
-                    while (!deque.isEmpty()) {
-                        sb.append(deque.pollFirst());
-                        if (deque.isEmpty())
-                            break;
-                        sb.append(",");
-                    }
-
-                } else {
-                    while (!deque.isEmpty()) {
-                        sb.append(deque.pollLast());
-                        if (deque.isEmpty())
-                            break;
-                        sb.append(",");
-                    }
+                if(deque.pollLast() == null) {
+                    sb.append("error\n");
+                    return;
                 }
-                sb.append("]").append("\n");
             }
         }
-        System.out.println(sb);
+
+        makePrintString(forward_direction);
     }
-}
+
+    private static void makePrintString(boolean forward_direction) {
+        sb.append('[');
+
+        if(deque.size() > 0) {
+            if(forward_direction) {
+                sb.append(deque.pollFirst());
+
+                while(!deque.isEmpty()) {
+                    sb.append(',').append(deque.pollFirst());
+                }
+            } else {
+                sb.append(deque.pollLast());
+
+                while(!deque.isEmpty()) {
+                    sb.append(',').append(deque.pollLast());
+                }
+            }
+        }
+
+        sb.append(']').append('\n');
+    } // End of makePrintString
+} // End of class
