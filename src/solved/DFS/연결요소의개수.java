0f4b80eea0ea8be013a509434eeb3a4bc7f51775
kj <jun111haha@gmail.com>
2022-10-13 00:23:30 +0900

    10.13

---
+++ src/solved/DFS/연결요소의개수.java
@@ -0,0 +1,52 @@
+package solved.DFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 연결요소의개수 {
+    static int N;
+    static int M;
+
+    //노드
+    static int arr[][];
+    //방문여부
+    static boolean visit[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(str.nextToken());
+        M = Integer.parseInt(str.nextToken());
+
+        arr = new int[N + 1][N + 1];
+        visit = new boolean[N + 1];
+        for(int i =0; i<M; i++){
+            StringTokenizer str1 = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(str1.nextToken());
+            int y = Integer.parseInt(str1.nextToken());
+            arr[x][y] = arr[y][x] = 1;
+        }
+
+        int cnt = 0;
+        for(int i =1; i<=N; i++){
+            if(visit[i] == false){
+                dfs(i);
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    public static void dfs(int start){
+        visit[start] = true;
+
+        for(int i =1; i<=N; i++){
+            if(arr[start][i] == 1 && visit[i] == false){
+                dfs(i);
+            }
+        }
+    }
+}
