02e6826816e1f7ca02468470b095b438398ddca9
kj <jun111haha@gmail.com>
2022-10-17 23:30:31 +0900

    10.17

---
+++ src/solved/DFS/촌수계산.java
@@ -0,0 +1,19 @@
+package solved.DFS;
+
+public class 촌수계산 {
+    int N;
+    int M;
+
+    //노드
+    static int arr[][];
+    //방문여부
+    static boolean visit[][];
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void dfs(int start){
+
+    }
+}
