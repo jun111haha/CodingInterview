ae1b8b2e991edd44fef0b0eb949627ade4b99d38
kj <jun111haha@gmail.com>
2022-10-19 00:09:47 +0900

    10.18

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(factorial(1));
+    static boolean[] check;
+    static int[][] arr;
+    static  StringBuilder sb = new StringBuilder();
+
+    static int node, line, start;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        node = Integer.parseInt(st.nextToken());
+        line = Integer.parseInt(st.nextToken());
+        start = Integer.parseInt(st.nextToken());
+        arr = new int[node + 1][node +1];
+        check = new boolean[node + 1];
+
+        for(int i =0; i<line; i++){
+            StringTokenizer str = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(str.nextToken());
+            int b = Integer.parseInt(str.nextToken());
+
+            arr[a][b] = arr[b][a] =  1;
+        }
+        dfs(start);
+        System.out.println(sb);
+
     }
 
-    public static int factorial(int i){
+    public static void dfs(int start){
+        check[start] = true;
+        sb.append(start + " ");
 
-        if(i == 100) {
-            System.out.println("재귀 종료");
-            return 0;
+        for(int i =1; i<=node; i++){
+            if(arr[start][i] == 1 && !check[i]){
+                dfs(i);
+            }
         }
-
-        System.out.println(i);
-        return factorial(i + 1);
-
     }
 }
