9b78051f26b9a32dff1fb17a2895ed799566a354
kj <jun111haha@gmail.com>
2022-11-15 00:25:49 +0900

    11.15

--- src/Programmers/level2/카펫.java
+++ src/Programmers/level2/카펫.java
@@ -7,9 +7,11 @@ public class 카펫 {
         System.out.println(solution(brown, yellow));
     }
 
+    /*
+    * 완전탐색 브루트포스
+    * */
     public static int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-
         int sum = brown + yellow; //전체 갯수
 
         //행과 열의 갯수는 3이상이여야 합니다.
@@ -25,7 +27,7 @@ public class 카펫 {
             //행은 열보다 크거나 같아야 합니다.
             if(row>= col) {
                 //문제의 규칙에 의하면 row-2*col-2 = yellow 입니다.
-                if((row-2) * (col-2) == yellow) {
+                if((row - 2) * (col - 2) == yellow) {
                     answer[0] = row;
                     answer[1] = col;
                     return answer;
