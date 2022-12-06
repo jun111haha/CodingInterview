2703f0c5a17ccfa197b982c5649d72761b1d687c
kj <jun111haha@gmail.com>
2022-12-07 00:46:26 +0900

    12.06

---
+++ src/solved/DP/설탕배달.java
@@ -0,0 +1,34 @@
+package solved.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+* 2839 번
+* 18
+* */
+public class 설탕배달 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        while (true){
+            //5 로 나눠지면
+            if(n % 5 == 0){
+                cnt += n / 5;
+                System.out.println(cnt);
+                break;
+            //3 일 경우?
+            }else{
+                n -= 3;
+                cnt ++;
+            }
+            if(n < 0){
+                System.out.println("-1");
+                break;
+            }
+        }
+
+    }
+}
