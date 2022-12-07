06321f13ef7b3bc83984b684ec8bc9b888b07254
kj <jun111haha@gmail.com>
2022-12-08 00:33:06 +0900

    12.07

--- src/solved/DP/설탕배달.java
+++ src/solved/DP/설탕배달.java
@@ -14,12 +14,12 @@ public class 설탕배달 {
         int n = Integer.parseInt(br.readLine());
         int cnt = 0;
         while (true){
-            //5 로 나눠지면
+            //5 킬로그램 봉지 일경우 5로 나눠서 떨어지면
             if(n % 5 == 0){
                 cnt += n / 5;
                 System.out.println(cnt);
                 break;
-            //3 일 경우?
+            //3 킬로그램 봉지 일 경우?
             }else{
                 n -= 3;
                 cnt ++;
