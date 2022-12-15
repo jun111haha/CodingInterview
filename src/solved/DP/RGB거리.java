5bf72cec9aa473ba3ab4d9d48a7b8d1b61122d35
kj <jun111haha@gmail.com>
2022-12-15 21:21:30 +0900

    12.15

--- src/solved/DP/RGB거리.java
+++ src/solved/DP/RGB거리.java
@@ -3,8 +3,17 @@ package solved.DP;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/*
+3
+26 40 83
+49 60 57
+13 89 99
+before [[26, 40, 83], [49, 60, 57], [13, 89, 99]]
+after [[26, 40, 83], [89, 143, 83], [96, 172, 188]]
+* */
 public class RGB거리 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,6 +31,7 @@ public class RGB거리 {
             color[i][Blue] = Integer.parseInt(st.nextToken());
         }
 
+        System.out.println("before " + Arrays.deepToString(color));
         for(int i =1; i<testCase; i++){
             color[i][Red] += Math.min(color[i - 1][Blue], color[i - 1][Green]);
             color[i][Green] += Math.min(color[i - 1][Blue], color[i  - 1][Blue]);
@@ -29,6 +39,7 @@ public class RGB거리 {
         }
 
         int colorSum = Math.min(Math.min(color[testCase - 1][Red], color[testCase - 1][Green]), color[testCase - 1][Blue]);
+        System.out.println("after " + Arrays.deepToString(color));
         System.out.println(colorSum);
 
     }
