36d7604208615f361bcfcb2e1659c0f035e37436
kj <jun111haha@gmail.com>
2023-01-09 23:33:35 +0900

    1.8

--- src/solved/자료구조/silver/학생인기도측정.java
+++ src/solved/자료구조/silver/학생인기도측정.java
@@ -3,12 +3,12 @@ package solved.자료구조.silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedHashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class 학생인기도측정 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int tc = Integer.parseInt(br.readLine());
 
         LinkedHashMap<String, Integer> hashMap = new LinkedHashMap<>();
@@ -26,7 +26,24 @@ public class 학생인기도측정 {
                 hashMap.put(temp, hashMap.getOrDefault(temp, 0) + 1);
             }
         }
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(hashMap.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if(o1.getValue() > o2.getValue()){
+                    return -1;
+                }else if(o1.getValue() < o2.getValue()){
+                    return 1;
+                }
 
+                return o1.getKey().compareTo(o2.getKey());
+            }
+        });
 
+        for(int i =0; i<list.size(); i++){
+            String temp = String.valueOf(list.get(i));
+            sb.append(temp.replaceAll("=", " ") + "\n");
+        }
+        System.out.println(sb);
     }
 }
