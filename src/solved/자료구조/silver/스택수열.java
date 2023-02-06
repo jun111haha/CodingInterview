a5cd15404471218d8b0689172f5a82f62ff8b01c
kj <jun111haha@gmail.com>
2023-02-06 23:39:56 +0900

    2.06

--- src/solved/자료구조/silver/스택수열.java
+++ src/solved/자료구조/silver/스택수열.java
@@ -8,9 +8,33 @@ import java.util.Stack;
 public class 스택수열 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        boolean isCheck = false;
         int tc = Integer.parseInt(br.readLine());
 
         Stack<Integer> stack = new Stack<>();
+        int j = 1;
+        for(int i=0; i<tc; i++){
+            int N = Integer.parseInt(br.readLine());
+            for(; j<=N; j++){
+                stack.push(j);
+                sb.append("+" + "\n");
+            }
+
+            if(stack.peek() == N){
+                stack.pop();
+                sb.append("-" + "\n");
+            }else{
+                isCheck = true;
+            }
+        }
+
+        if(isCheck){
+            System.out.println("NO");
+        }else{
+            System.out.println(sb);
+
+        }
 
     }
 }
