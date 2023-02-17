6d187ef14728357e88e01080b4ad067b6502dad3
kj <jun111haha@gmail.com>
2023-02-17 23:35:01 +0900

    2.15

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        int [] arr = new int[N];
-        int [] dp = new int[11];
-        for(int i =0; i<N; i++){
-            arr[i] = Integer.parseInt(br.readLine());
+        Deque<int[]> q = new ArrayDeque<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
-        for(int i =0; i<arr.length; i++){
-            for(int j =4; j<=arr[i]; j++){
-                dp[j] = dp[j - 3] + dp[j - 2] + dp[j - 1];
-            }
-            sb.append(dp[arr[i]] + "\n");
+        StringBuilder sb = new StringBuilder();
+        sb.append("1 ");
+        int in = arr[0];
+
+        for(int i=1; i<n; i++){
+            q.add(new int[] {(i+1), arr[i]}); // {풍선 idx, 내용}
         }
-        System.out.println(sb);
+
+        for(int[] i : q){
+            System.out.println(Arrays.toString(i));
+        }
     }
 }
