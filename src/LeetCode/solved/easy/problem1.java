d8e332e24a75c4861ef7eba47daedc305112747a
kj <jun111haha@gmail.com>
2023-03-09 00:12:17 +0900

    3.05

---
+++ src/LeetCode/solved/easy/problem1.java
@@ -0,0 +1,32 @@
+package LeetCode.solved.easy;
+
+import java.util.ArrayList;
+
+public class problem1 {
+    public static void main(String[] args) {
+        problem1 easy1 = new problem1();
+        System.out.println(easy1.twoSum(new int[]{3,2,4}, 6));
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    list.add(i);
+                    list.add(j);
+                    break;
+                }
+            }
+        }
+
+        int[] arr = new int[list.size()];
+        int size = 0;
+        for(int i : list){
+            arr[size++] = i;
+        }
+
+        return arr;
+    }
+}
---
+++ src/LeetCode/solved/easy/problem9.java
@@ -0,0 +1,20 @@
+package LeetCode.solved.easy;
+
+public class problem9 {
+    public static void main(String[] args) {
+        problem9 problem9 = new problem9();
+        System.out.println(problem9.isPalindrome(121));
+    }
+
+    public boolean isPalindrome(int x) {
+        boolean isCheck = false;
+        StringBuffer sb = new StringBuffer(String.valueOf(x));
+        String temp = sb.reverse().toString();
+
+        if(temp.equals(String.valueOf(x))){
+           isCheck = true;
+        }
+
+        return isCheck;
+    }
+}
