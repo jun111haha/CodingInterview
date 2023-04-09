c62e39d214c518a9c1893be09ddd0898e16975c3
kj <jun111haha@gmail.com>
2023-04-09 23:12:36 +0900

    4.09

--- src/leetcode/easy/solved/easy14.java
+++ src/leetcode/easy/solved/easy14.java
@@ -8,12 +8,12 @@ public class easy14 {
     public static String longestCommonPrefix(String[] strs) {
         String ans = strs[0];
         for(int i=1; i<strs.length; i++){
+            System.out.println(strs[i].indexOf(ans));
             while (strs[i].indexOf(ans) != 0){
                 ans = ans.substring(0, strs.length - 1);
             }
         }
 
-        System.out.println("flow".indexOf("flower"));
         return ans;
     }
 }
